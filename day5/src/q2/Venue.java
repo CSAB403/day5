888b851b36f83e873a020c43785b0a0476dfa3b9
CSAB403 <77146587+CSAB403@users.noreply.github.com>
2021-08-27 16:38:23 +0530

    Add files via upload

---
+++ day5/src/q2/Venue.java
@@ -0,0 +1,33 @@
+package q2;
+
+import java.util.Scanner;
+
+import q1.Player;
+
+public class Venue {
+	String name;
+	String city;
+	public Venue(String name,String city) {
+		this.name=name;
+		this.city=city;
+	}
+	public void VenueDetails(){
+		System.out.println("venue details:");
+		System.out.println("venue name:"+name);
+		System.out.println("venue city"+city);
+	}
+public static void main(String[] args) {
+Scanner sc= new Scanner(System.in);
+System.out.println("enbter  the venue name");
+	String name=sc.nextLine();
+	System.out.println("enbter  the player city");
+	String  city=sc.nextLine();
+	Venue obj=new Venue(name,city);
+	obj.VenueDetails();
+
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
